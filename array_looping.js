2155fb541236541a55d860f2bb0e21d694ca33d7
Badsha012 <badshasheikh361@gmail.com>
2025-08-05 09:32:37 -0700

    six commit

--- array_looping.js
+++ array_looping.js
@@ -2,7 +2,13 @@
 
 const fruits=['apple','banana','anar','watermelon','daav'];
 
-for(let i=0;i<10;i++){
-    console.log(i);
+for(let i=0;i<fruits.length;i++){
+    // console.log(i);
     console.log(fruits[i]);
+}
+
+
+const n=[12,13,14,15,19,66,8,9];
+for(let j=0;j<n.length;j++){
+    console.log(n[j]);
 }
---
+++ array_while.js
@@ -0,0 +1,8 @@
+
+const num=[1,2,3,4,5,6,7,8,9];
+let k=0;
+while(k<num.length){
+    console.log(num[k])
+    k++;
+
+}
