0e8b7eaa652f623c2b162b56f5982de510b27097
Badsha012 <badshasheikh361@gmail.com>
2025-08-06 01:48:43 -0700

    nine commit

---
+++ sort.js
@@ -0,0 +1,14 @@
+
+
+const number=[6,8,2,3,5];
+
+
+console.log(number);
+number.sort();
+console.log(number);
+
+
+const friend=['vin','rupa','guma'];
+console.log(friend);
+friend.sort();
+console.log(friend);
