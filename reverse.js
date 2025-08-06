79e433d5d329ffe826728cef405980eede227005
Badsha012 <badshasheikh361@gmail.com>
2025-08-05 20:34:26 -0700

    seven commit

---
+++ reverse.js
@@ -0,0 +1,21 @@
+  const n=[1,2,3,4,5,6,7,8,9,11,12,13];
+
+
+
+  const r=[];
+  for(let i=0;i<n.length;i++){
+    // console.log(n[i]);
+    r.unshift(n[i]);
+
+  }
+  console.log(r);
+  
+
+  const number=[1,2,3,4,5];
+
+
+const rev=[];
+const result=number.reverse();
+console.log(result);
+
+
