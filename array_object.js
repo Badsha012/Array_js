cd55ccc09e7c55c8671108e5c937bedf066ace18
Badsha012 <badshasheikh361@gmail.com>
2025-08-06 22:43:48 -0700

    nine commit

---
+++ array_object.js
@@ -0,0 +1,11 @@
+const numbers=[1,2,3,4,5];
+const players=[{},{},{}];
+const employes=[{
+    name:'min',designation:'content Writer',salary:25000},
+    {name:'Badsha',designation:'digita marketer',salary:21000},
+    
+]
+// /console.log(employes[0].salary);
+for(const employee of employes){
+    console.log(employee.salary,employee.name);
+}
